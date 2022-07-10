fa26091e84613843df5f539e5f69e8f3055f5712
Nikita Grishchenkov <nikita@grishchenkov.ru>
2022-07-10 16:31:35 +0300

    feat: split Qt design files
    #7

---
+++ SOURCES/generate_py.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python -m PyQt5.uic.pyuic -x "RNN/Main.ui" -o "../View/RNN/MainWindow.py"
+python -m PyQt5.uic.pyuic -x "TSAC/Main.ui" -o "../View/TSAC/MainWindow.py"
+
+python -m PyQt5.uic.pyuic -x "RNN/Database.ui" -o "../View/RNN/DatabaseWindow.py"
+python -m PyQt5.uic.pyuic -x "TSAC/Database.ui" -o "../View/TSAC/DatabaseWindow.py"
+
+python -m PyQt5.uic.pyuic -x "RNN/Administrator.ui" -o "../View/RNN/AdministratorWindow.py"
+python -m PyQt5.uic.pyuic -x "TSAC/Administrator.ui" -o "../View/TSAC/AdministratorWindow.py"
+
+python -m PyQt5.uic.pyuic -x Login.ui -o "../View/LoginWindow.py"
+python -m PyQt5.uic.pyuic -x DataLoad.ui -o "../View/DataLoadWindow.py"
