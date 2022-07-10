7ccfc769ddf8c768d1c0a91a043f4f04495f3adc
Nahobi no Kami <63340379+Nahobi-no-Kami@users.noreply.github.com>
2022-07-11 00:55:48 +0300

    1.1
    Убрана дата с интерфейса, загрузка данных происходит по всему временному интервалу, подправлен скрипт генерации интерфейса

--- SOURCES/generate_py.sh
+++ SOURCES/generate_py.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python -m PyQt5.uic.pyuic -x "RNN/Main.ui" -o "../View/RNN/MainWindow.py"
-python -m PyQt5.uic.pyuic -x "TSAC/Main.ui" -o "../View/TSAC/MainWindow.py"
+python -m PyQt5.uic.pyuic -x "RNN/Main.ui" -o "../VIEWS/RNN/MainWindow.py"
+python -m PyQt5.uic.pyuic -x "TSAC/Main.ui" -o "../VIEWS/TSAC/MainWindow.py"
 
-python -m PyQt5.uic.pyuic -x "RNN/Database.ui" -o "../View/RNN/DatabaseWindow.py"
+python -m PyQt5.uic.pyuic -x "RNN/Database.ui" -o "../VIEWS/RNN/DatabaseWindow.py"
 
-python -m PyQt5.uic.pyuic -x "RNN/Administrator.ui" -o "../View/RNN/AdministratorWindow.py"
-python -m PyQt5.uic.pyuic -x "TSAC/Administrator.ui" -o "../View/TSAC/AdministratorWindow.py"
+python -m PyQt5.uic.pyuic -x "RNN/Administrator.ui" -o "../VIEWS/RNN/AdministratorWindow.py"
+python -m PyQt5.uic.pyuic -x "TSAC/Administrator.ui" -o "../VIEWS/TSAC/AdministratorWindow.py"
