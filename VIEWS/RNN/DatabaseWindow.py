2a99390b51d4298a640a33c57334fe707b56dfa7
Nikita Grishchenkov <nikita@grishchenkov.ru>
2022-07-10 16:34:28 +0300

    feat: split python design files
    #7

--- VIEWS/RNN/DatabaseWindow.py
+++ VIEWS/RNN/DatabaseWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Database.ui'
+# Form implementation generated from reading ui file 'RNN/Database.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
