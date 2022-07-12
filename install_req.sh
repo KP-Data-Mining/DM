7405551c3d6ebac36e2c5c86a780cb37ff52a275
Nikita Grishchenkov <nikita@grishchenkov.ru>
2022-07-12 11:30:11 +0300

    ci: add scripts to install dependencies

---
+++ install_req.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install colorama
+pip install PyQt5
+pip install pandas
+pip install matplotlib
+pip install tensorflow
+pip install pandastable
+pip install umap
+pip install umap-learn
+pip install tsfresh
+pip install bokeh
+pip install pypiwin32
+pip install PyQtWebEngine
