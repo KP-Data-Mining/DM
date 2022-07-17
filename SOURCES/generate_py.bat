865f8ca9e43ad272acebeedf87c1613ac506b884
Nikita Grishchenkov <nikita@grishchenkov.ru>
2022-07-17 13:59:02 +0300

    feat: update Qt design files for progress bars

--- SOURCES/generate_py.bat
+++ SOURCES/generate_py.bat
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python -m PyQt5.uic.pyuic -x ProgressBar.ui -o "../VIEWS/ProgressBar.py"
+
 python -m PyQt5.uic.pyuic -x RNN/Main_Ru.ui -o "../VIEWS/RNN/MainWindow_Ru.py"
 python -m PyQt5.uic.pyuic -x RNN/Main_Eng.ui -o "../VIEWS/RNN/MainWindow_Eng.py"
 python -m PyQt5.uic.pyuic -x TSAC/Main_Ru.ui -o "../VIEWS/TSAC/MainWindow_Ru.py"
